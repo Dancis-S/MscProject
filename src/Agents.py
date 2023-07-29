0b62e13a0a837b1fab368e51f316b87c92d6a8cb
up958769 <dsindilaisme@gmail.com>
2023-07-29 01:01:01 +0100

    Refactoring

--- src/Agents.py
+++ src/Agents.py
@@ -4,13 +4,19 @@ import random
 
 
 class RandomAgent:
+    """
+    Class that contain code for the random agent
+    """
     def __init__(self, player_id):
-        self.id = player_id
+        self.player_id = player_id
 
     def get_action(self, game):
-        board = game.get_my_board(self.id)
+        """
+        Given a game state, returns a random move to play
+        """
+        board = game.get_my_board(self.player_id)
         open_positions = board.open_positions
-        stack = game.get_my_stack(self.id)
+        stack = game.get_my_stack(self.player_id)
         shop = game.shop
 
         chosen_tile = random.choice(range(len(stack)))
@@ -20,19 +26,31 @@ class RandomAgent:
         # Chosen tile index, chosen position index, tile shop index
         return chosen_location, chosen_tile, chosen_shop
 
+    def my_id(self):
+        """
+        Prints the agents id
+        """
+        print(self.player_id)
+
 
 class GreedyAgentRandom:
+    """
+    Class for the greedy agent
+    """
     def __init__(self, player_id):
-        self.id = player_id
+        self.player_id = player_id
 
     def get_action(self, game):
+        """
+        Given a game state returns the best move, using the greedy approach
+        """
         # Check the open positions
         # Try all possible combinations and return the move that maxes possible increase
         # Otherwise make a random move!
         highest_increase = -1
         best_move = None  # The best possible move, shop is randomised though
-        board = game.get_my_board(self.id)
-        my_stack = game.get_my_stack(self.id)
+        board = game.get_my_board(self.player_id)
+        my_stack = game.get_my_stack(self.player_id)
         open_positions = board.open_positions
 
         for position in open_positions:
@@ -47,3 +65,9 @@ class GreedyAgentRandom:
                     best_move = (position, tile)
 
         return best_move[0], my_stack.index(best_move[1]), random.choice(range(3))
+
+    def my_id(self):
+        """
+        Prints the agents id
+        """
+        print(self.player_id)
